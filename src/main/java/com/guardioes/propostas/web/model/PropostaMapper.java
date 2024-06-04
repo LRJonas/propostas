c238e84a42776bbb8c8a1a26735778d3961200a5
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-04 19:37:30 -0300

    feat: Implementação dos Mapper específicos para a classe Proposta e remoção do Mapper genérico

---
+++ src/main/java/com/guardioes/propostas/web/model/PropostaMapper.java
@@ -0,0 +1,24 @@
+package com.guardioes.propostas.web.model;
+
+import com.guardioes.propostas.entity.Proposta;
+import com.guardioes.propostas.web.dto.PropostaCreateDto;
+import com.guardioes.propostas.web.dto.PropostaResponseDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+public class PropostaMapper {
+    public static Proposta paraProposta(PropostaCreateDto dto) {
+        Proposta proposta = new ModelMapper().map(dto, Proposta.class);
+        proposta.setFuncionarioId(1L);
+        return proposta;
+    }
+
+    public static PropostaResponseDto paraDto(Proposta proposta) {
+        PropostaResponseDto dto = new ModelMapper().map(proposta, PropostaResponseDto.class);
+        dto.setFuncionarioNome("Nome");
+        dto.setFuncionarioCpf("Cpf");
+        return dto;
+    }
+}
