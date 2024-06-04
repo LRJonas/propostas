19072f49112f2e78d655301e548da7aea038db59
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-04 19:59:34 -0300

    feat: implementação do endpoint de criação de Proposta

--- src/main/java/com/guardioes/propostas/service/PropostaService.java
+++ src/main/java/com/guardioes/propostas/service/PropostaService.java
@@ -1,12 +1,19 @@
 package com.guardioes.propostas.service;
 
+import com.guardioes.propostas.entity.Proposta;
 import com.guardioes.propostas.repository.PropostaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PropostaService {
 
     private final PropostaRepository propostaRepository;
+
+    @Transactional
+    public Proposta criar(Proposta proposta) {
+        return propostaRepository.save(proposta);
+    }
 }
--- src/main/java/com/guardioes/propostas/web/controller/PropostaController.java
+++ src/main/java/com/guardioes/propostas/web/controller/PropostaController.java
@@ -1,13 +1,26 @@
 package com.guardioes.propostas.web.controller;
 
+import com.guardioes.propostas.entity.Proposta;
 import com.guardioes.propostas.service.PropostaService;
+import com.guardioes.propostas.web.dto.PropostaCreateDto;
+import com.guardioes.propostas.web.dto.PropostaResponseDto;
+import com.guardioes.propostas.web.model.PropostaMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/propostas")
 public class PropostaController {
 
     private final PropostaService propostaService;
 
+    @PostMapping
+    public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaCreateDto dto) {
+        Proposta proposta = propostaService.criar(PropostaMapper.paraProposta(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(PropostaMapper.paraDto(proposta));
+    }
 }
+
