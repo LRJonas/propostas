f9943c303d67cd941438c076a01cb36bd813fc44
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-04 19:19:56 -0300

    fix: correção da nomenclatura do Mapper genérico

--- src/main/java/com/guardioes/propostas/web/model/Mapper.java
+++ src/main/java/com/guardioes/propostas/web/model/Mapper.java
@@ -5,11 +5,11 @@ import org.modelmapper.ModelMapper;
 import java.util.List;
 
 public class Mapper {
-    public static <O, D> D paraDto(O origin, Class<D> destination) {
+    public static <O, D> D paraClasse(O origin, Class<D> destination) {
         return new ModelMapper().map(origin, destination);
     }
 
-    public static <O, D> List<D> paraListaDto(List<O> origin, Class<D> destination) {
-        return origin.stream().map(o -> paraDto(o, destination)).toList();
+    public static <O, D> List<D> paraLista(List<O> origin, Class<D> destination) {
+        return origin.stream().map(o -> paraClasse(o, destination)).toList();
     }
 }
