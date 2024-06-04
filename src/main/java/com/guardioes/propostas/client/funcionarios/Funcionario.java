c53ead4a38f51edfed1f5042e9f8b710aa3e8b60
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-04 17:36:19 -0300

    feat: criação da entidade Funcionario

---
+++ src/main/java/com/guardioes/propostas/client/funcionarios/Funcionario.java
@@ -0,0 +1,20 @@
+package com.guardioes.propostas.client.funcionarios;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Funcionario {
+    @Id
+    private Long id;
+    private String cpf;
+    private String nome;
+    private String cargo;
+    private boolean ativo;
+}
