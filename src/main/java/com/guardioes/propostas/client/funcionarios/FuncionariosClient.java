aa0d3bb766bcb4b14747a07c468a80aedc7154a3
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-06 08:23:31 -0300

    fix: Alteração da porta de conexão com o gateway para padrão do gateway

--- src/main/java/com/guardioes/propostas/client/funcionarios/FuncionariosClient.java
+++ src/main/java/com/guardioes/propostas/client/funcionarios/FuncionariosClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "funcionarios", url = "http://localhost:8081/api/v1/funcionarios")
+@FeignClient(name = "funcionarios", url = "http://localhost:8080/api/v1/funcionarios")
 public interface FuncionariosClient {
 
     @GetMapping("/cpf/{cpf}")
