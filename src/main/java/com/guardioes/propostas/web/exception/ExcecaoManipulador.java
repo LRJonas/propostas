e9a3e0bf105ab15cb85e02f98f0d0d2bd7a610d9
Joao Pedro <joaopedrogolenia@gmail.com>
2024-06-06 14:25:24 -0300

    Feat: Tratndo exceções com o manipulador

--- src/main/java/com/guardioes/propostas/web/exception/ExcecaoManipulador.java
+++ src/main/java/com/guardioes/propostas/web/exception/ExcecaoManipulador.java
@@ -1,7 +1,6 @@
 package com.guardioes.propostas.web.exception;
 
-import com.guardioes.propostas.exception.ExcecaoCpfDuplicado;
-import com.guardioes.propostas.exception.ExcecaoPropostaInexistente;
+import com.guardioes.propostas.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,6 +22,7 @@ public class ExcecaoManipulador extends ResponseEntityExceptionHandler {
                 .body(new MensagemErro(request, HttpStatus.CONFLICT, ex.getMessage()));
     }
 
+
     @ExceptionHandler(ExcecaoPropostaInexistente.class)
     public final ResponseEntity<MensagemErro> excecaoPropostaInexistente(ExcecaoPropostaInexistente ex, HttpServletRequest request) {
         log.error("Erro na API", ex);
@@ -30,5 +30,35 @@ public class ExcecaoManipulador extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new MensagemErro(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+
+//    @ExceptionHandler(ExcecaoFuncionarioInvalido.class)
+//    public final ResponseEntity<MensagemErro> excecaoFuncionarioInvalido(ExcecaoFuncionarioInvalido ex, HttpServletRequest request) {
+//        log.error("Erro na API", ex);
+//        return ResponseEntity
+//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(new MensagemErro(request, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
+//    }
+
+
+    @ExceptionHandler(ExcecaoPropostaInativa.class)
+    public final ResponseEntity<MensagemErro> excecaoPropostaInativa(ExcecaoPropostaInativa ex, HttpServletRequest request) {
+        log.error("Erro na API", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new MensagemErro(request, HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage()));
+    }
+
+
+    @ExceptionHandler(ExcecaoVotoInvalido.class)
+    public final ResponseEntity<MensagemErro> excecaoVotoInvalido(ExcecaoVotoInvalido ex, HttpServletRequest request) {
+        log.error("Erro na API", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new MensagemErro(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
     }
 }
