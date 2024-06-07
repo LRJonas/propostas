77da4da6ade6b676469f85282fdd7a461a8ba73c
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-07 17:51:43 -0300

    fix: Conflito banco de dados

--- src/main/java/com/guardioes/propostas/web/exception/ExcecaoManipulador.java
+++ src/main/java/com/guardioes/propostas/web/exception/ExcecaoManipulador.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.net.ConnectException;
-
 @Slf4j
 @RestControllerAdvice
 public class ExcecaoManipulador extends ResponseEntityExceptionHandler {
