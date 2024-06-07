df4e70412fdfbf5d02154dcaf58e4b8405af5815
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-07 17:18:26 -0300

    feat: implementação de anotações do validation

--- src/main/java/com/guardioes/propostas/web/dto/PropostaCreateDto.java
+++ src/main/java/com/guardioes/propostas/web/dto/PropostaCreateDto.java
@@ -1,16 +1,26 @@
 package com.guardioes.propostas.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PropostaCreateDto {
+    @NotBlank(message = "O título é obrigatório.")
     private String titulo;
+
+    @NotBlank(message = "A descrição é obrigatória.")
     private String descricao;
+
+    @NotBlank
+    @CPF(message = "O CPF do funcionário é inválido.")
+    @Size(min = 11, max = 11, message = "O CPF do funcionário deve ter exatamente 11 caracteres.")
     private String funcionarioCpf;
 }
